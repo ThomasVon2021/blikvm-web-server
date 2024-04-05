e42dc15ebe3de7e581f2bce6ba00401bde0795bb
ThomasVon2021 <1767893964@qq.com>
2024-04-05 20:09:00 +0800

    docs: add HardwareType comment

--- src/common/enums.js
+++ src/common/enums.js
@@ -1,6 +1,22 @@
+/**
+ * Represents the hardware types.
+ * @enum {number}
+ */
 export const HardwareType = {
+  /**
+   * Unknown hardware type.
+   */
   UNKNOW: 0,
+  /**
+   * Raspberry Pi 4B.
+   */
   PI4B: 1,
+  /**
+   * Compute Module 4.
+   */
   CM4: 2,
+  /**
+   * MangoPi hardware.
+   */
   MangoPi: 3
 };
