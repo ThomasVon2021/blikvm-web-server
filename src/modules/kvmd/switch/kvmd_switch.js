5c80421837be8b71e5aa74d92f2b8d342971285d
Jonathan Taylor <taylorjonl@gmail.com>
2024-12-27 19:25:48 -0700

    Added support for Tesmart KVM switches

--- src/modules/kvmd/switch/kvmd_switch.js
+++ src/modules/kvmd/switch/kvmd_switch.js
@@ -21,6 +21,8 @@
 *****************************************************************************/
 import KVMDBliSwitchV1 from './kvmd_switch_v1.js';
 import KVMDBliSwitchV2 from './kvmd_switch_v2.js';
+import KVMDTesmartHSW0801Switch from './kvmd_switch_tesmart_hsw0801.js';
+import KVMDTesmartHSW1601Switch from './kvmd_switch_tesmart_hsw1601.js';
 import Logger from '../../../log/logger.js';
 import {
   SWITCH_PATH,
@@ -49,6 +51,12 @@ class KVMSwitchFactory {
         break;
       case SwitchModulesID.BliKVM_switch_v2:
         this._instance = new KVMDBliSwitchV2();
+        break;
+      case SwitchModulesID.TESmart_HSW0801_switch:
+        this._instance = new KVMDTesmartHSW0801Switch();
+        break;
+      case SwitchModulesID.TESmart_HSW1601_switch:
+        this._instance = new KVMDTesmartHSW1601Switch();
         break;
       default:
         logger.error(`Unknown switch type: ${switchId}`);
