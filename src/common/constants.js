90eface8dc58c9cfc52d3575f6efb1b2dcf3b000
ThomasVon2021 <1767893964@qq.com>
2025-03-01 07:27:14 +0000

    fix:after a reboot a user session to the webUI is still authenticated

--- src/common/constants.js
+++ src/common/constants.js
@@ -20,6 +20,7 @@
 #                                                                            #
 *****************************************************************************/
 import dotenv from 'dotenv';
+import crypto from 'crypto';
 dotenv.config();
 
 export const CONFIG_PATH = process.argv[2] ? process.argv[2] : 'config/app.json';
@@ -27,4 +28,4 @@ console.log(`Config path is: ${CONFIG_PATH}`);
 export const UTF8 = 'utf8';
 export const BliKVMSwitchV1ModuleName = 'BliKVM_switch_v1';
 export const BliKVMSwitchV2ModuleName = 'BliKVM_switch_v2';
-export const JWT_SECRET = process.env.JWT_SECRET || 'helloBliKVM';
+export const JWT_SECRET = crypto.randomBytes(32).toString('hex');
